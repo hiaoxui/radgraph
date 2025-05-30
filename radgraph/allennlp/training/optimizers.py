1ab10281b282dc32f975b756c5c9f263d75d338e
Guanghui Qin <hiaoxu.i@gmail.com>
2025-05-29 17:38:49 -0700

    remove huggingface adamw optimizer

--- radgraph/allennlp/training/optimizers.py
+++ radgraph/allennlp/training/optimizers.py
@@ -293,33 +293,6 @@ class AdamWOptimizer(Optimizer, torch.optim.AdamW):
             amsgrad=amsgrad,
         )
 
-
-@Optimizer.register("huggingface_adamw")
-class HuggingfaceAdamWOptimizer(Optimizer, transformers.AdamW):
-    """
-    Registered as an `Optimizer` with name "huggingface_adamw".
-    """
-
-    def __init__(
-        self,
-        model_parameters: List[Tuple[str, torch.nn.Parameter]],
-        parameter_groups: List[Tuple[List[str], Dict[str, Any]]] = None,
-        lr: float = 1e-5,
-        betas: Tuple[float, float] = (0.9, 0.999),
-        eps: float = 1e-08,
-        weight_decay: float = 0.0,
-        correct_bias: bool = True,
-    ):
-        super().__init__(
-            params=make_parameter_groups(model_parameters, parameter_groups),
-            lr=lr,
-            betas=betas,
-            eps=eps,
-            weight_decay=weight_decay,
-            correct_bias=correct_bias,
-        )
-
-
 @Optimizer.register("adagrad")
 class AdagradOptimizer(Optimizer, torch.optim.Adagrad):
     """
