836934c3897819617b07d4b5969f740d37c59a3f
Guanghui Qin <guanghuiqin@microsoft.com>
2025-05-29 18:41:02 -0700

    ray worker

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-from radgraph import RadGraph, F1RadGraph
+from radgraph import RadGraphRewardServer
 
-refs = [
-    "no acute cardiopulmonary abnormality",
-    "et tube terminates 2 cm above the carina retraction by several centimeters is recommended for more optimal placement bibasilar consolidations better assessed on concurrent chest ct",
-]
 
-hyps = [
-    "no acute cardiopulmonary abnormality",
-    "endotracheal tube terminates 2 5 cm above the carina bibasilar opacities likely represent atelectasis or aspiration",
-]
-f1radgraph = F1RadGraph(reward_level="all", cuda=0)
-mean_reward, reward_list, hypothesis_annotation_lists, reference_annotation_lists  = f1radgraph(hyps=hyps, refs=refs)
-rewards = list(zip(*reward_list))
-reward1, reward2 = rewards
-x = 1
+if __name__ == "__main__":
+    refs = [
+        "no acute cardiopulmonary abnormality",
+        "et tube terminates 2 cm above the carina retraction by several centimeters is recommended for more optimal placement bibasilar consolidations better assessed on concurrent chest ct",
+    ] * 10
+    hyps = [
+        "no acute cardiopulmonary abnormality",
+        "endotracheal tube terminates 2 5 cm above the carina bibasilar opacities likely represent atelectasis or aspiration",
+    ] * 10
 
+    server = RadGraphRewardServer(num_workers=2)
+    rewards = server.compute_rewards(hyps, refs)
+    for i, reward in enumerate(rewards):
+        print(f"Pair {i+1} Rewards: {reward}")
